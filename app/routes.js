c8cd6f31552febf31ffa9bf688df4c04f310e217
Eric Huppert <eric@erichuppert.com>
2014-02-08 18:02:58 -0500

    curly brace fix

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app){ 
 	// Connection test
 	app.get('/ping', function(req, res){
-		res.json(message: 'Success!')
+		res.json({message: 'Success!'})
 	});
 
 	var controllers = require('./controllers')(app);
