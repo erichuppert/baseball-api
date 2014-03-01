1705017be0e3963b675810777659ce0a6545d5ba
eric <eric@erichuppert.com>
2014-02-28 20:14:41 -0800

    latest

--- app/controllers/players.js
+++ app/controllers/players.js
@@ -1,13 +1,24 @@
-module.exports = function(app, connection) {
+	module.exports = function(app, connection) {
 
 	// get players from query string
 	app.get('/players', function(req, res){
 		var name = req.query.name;
+		var positionType = req.query.positionType;
 		if (name) {
-			var sql = "SELECT * FROM player WHERE CONCAT(first, ' ', last) LIKE '%" + name + "%' LIMIT 10";
+			var sql = "SELECT * FROM player WHERE CONCAT(first, ' ', last) LIKE '%" + name + "%'";
+			if (positionType === 'pitcher') {
+				sql += " AND throws IS NOT NULL";
+			} else if (positionType === 'batter') {
+				sql +=  " AND bats IS NOT NULL";
+			}
+			sql += " LIMIT 10"
 			connection.query(sql, function(err, rows){
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				res.end(JSON.stringify(rows));
+				if (err) {
+					res.send(500, {status: 'error', message: err});
+					console.log(err);
+				} else {
+					res.send(200, rows);
+				}
 			})	
 		}
 	});
@@ -18,10 +29,9 @@ module.exports = function(app, connection) {
 		var sql = "SELECT * FROM player WHERE id=" + id + " LIMIT 1"
 		connection.query(sql, function(err, rows){
 			if (rows) {
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				res.end(JSON.stringify(rows[0]))
+				res.send(200, rows[0])
 			} else {
-				res.json({status: 'error', message: 'There is no player with that ID'})
+				res.send(200, {status: 'error', message: 'There is no player with that ID'})
 			}
 		})
 	});
--- server.js
+++ server.js
@@ -7,6 +7,17 @@ var database = require('./config/database');
 var connection = mysql.createConnection(database);
 connection.query('USE baseball');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', null);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.configure(function() {
+    app.use(allowCrossDomain);
+});
+
 var routes = require('./app/routes')(app, connection);
 
 app.listen(3000);
